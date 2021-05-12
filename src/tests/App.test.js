74933f01d6dc31ddc79d730aa1783479d73fd657
MistaRae <itsamistarae@gmail.com>
2021-05-12 18:33:00 +0100

    add working

---
+++ src/tests/App.test.js
@@ -0,0 +1,34 @@
+import { fireEvent, render } from '@testing-library/react';
+import App from '../App';
+
+describe('Calculator', () => {
+  let container;
+  beforeEach(() => {
+    container = render(<App/>)
+  })
+
+  it('should change running total on number enter', () => {
+    const button4 = container.getByText('4');
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button4);
+
+    expect(runningTotal).toHaveTextContent('4');
+  })
+
+  it('should add 1 to 4 and get 5', () => {
+    const button1 = container.getByText('1');
+    const button4 = container.getByText('4');
+    const equals = container.getByText('=')
+    const add = container.getByText('+')
+    const runningTotal = container.getByTestId('running-total');
+    fireEvent.click(button1);
+    fireEvent.click(add);
+    fireEvent.click(button4);
+    fireEvent.click(equals);
+
+    expect(runningTotal).toHaveTextContent('5');
+  })
+
+
+})
+
