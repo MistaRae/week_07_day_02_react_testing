c248b794ffb470d1b35e07386a199335a2a3b267
MistaRae <itsamistarae@gmail.com>
2021-05-12 21:43:28 +0100

    can chain operations

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -7,4 +7,29 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should update the display with the result of the running total', () => {
+    cy.get('#number2').click()
+    cy.get('#number3').click()
+    cy.get('.display').should('contain', '23')
+  })
+
+  it('should update the display with the result of the result of the operation', () => {
+    cy.get('#number2').click()
+    cy.get('#operator_add').click()
+    cy.get('#number3').click()
+    cy.get('#operator-equals').click()
+    cy.get('.display').should('contain', '5')
+  })
+
+  it('should be able to chain operations', () => {
+    cy.get('#number2').click()
+    cy.get('#operator_add').click()
+    cy.get('#number3').click()
+    cy.get('#operator-subtract').click()
+    cy.get('#number4').click()
+    cy.get('#operator-equals').click()
+    cy.get('.display').should('contain', '1')
+  })
+
 })
